d76c2df92ce8e9a34238d0e9755d9772599e48f2
Kevin Zhou <kzhou40@stuy.edu>
2025-09-24 14:52:41 -0400

    prelab take 2

--- ZhouK_Lab4_test_Mod_Sec.m
+++ ZhouK_Lab4_test_Mod_Sec.m
@@ -1,12 +1,12 @@
 clc; clear all;
 
-f1 = @(c1,c2,x1,x2) (c1 + c2 - 2);
+f1 = @(a) a(1) + a(2) - 2;
 
-f2 = @(c1,c2,x1,x2) (c1*x1 + c2*x2);
+f2 = @(b) b(1)*b(3) + b(2)*b(4);
 
-f3 = @(c1,c2,x1,x2) (c1)*(x1^2) + (c2)*(x2^2) - (2/3);
+f3 = @(c) c(1)*(c(3)^2) + c(2))*(c(4)^2 - (2/3);
 
-f4 = @(c1,c2,x1,x2) (c1)*(x1^3) +(c2)&(x2^3);
+f4 = @(d) d(1)*(d(3)^3) + d(2)*(d(4)^3);
 
 func_ary = {f1 f2 f3 f4};
 
@@ -20,6 +20,6 @@ x2 = 0.25;
 
 init_ary = [c1 c2 x1 x2];
 
-epsilon = 0.01;
+epsilon = 0.0001;
 
-a = ZhouK_Lab4(4, func_ary, init_ary, 0.01)
+a = ZhouK_Lab4(4, func_ary, init_ary, epsilon)
